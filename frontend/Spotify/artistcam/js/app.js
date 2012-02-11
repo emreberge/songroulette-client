695445c4b1d21f9474559a3b8f058fe41dcd295d
Ronald Pompa <pompa@spotify.com>
2012-02-12 00:26:54 +0100

    Added app init model.

--- frontend/Spotify/artistcam/js/app.js
+++ frontend/Spotify/artistcam/js/app.js
@@ -1,4 +1,8 @@
-"use strict"
+var sp = getSpotifyApi(1);
+var m = sp.require('sp://import/scripts/api/models');
 
-sp = getSpotifyApi(1);
+exports.init = init;
 
+function init() {
+    console.log("init()");
+}   
