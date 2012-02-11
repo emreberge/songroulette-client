3424c449a5b0e8b7a2898b0a373c2cd903a3e960
Petter Eriksson <periksson@periksson-mbp15-2.local>
2012-02-11 15:31:21 -0800

    Current track playing is now shown at the top.

--- frontend/Spotify/artistcam/js/app.js
+++ frontend/Spotify/artistcam/js/app.js
@@ -1,8 +1,33 @@
-var sp = getSpotifyApi(1);
-var m = sp.require('sp://import/scripts/api/models');
+sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var player = models.player;
 
 exports.init = init;
 
 function init() {
-    console.log("init()");
-}   
+
+    updatePageWithTrackDetails();
+
+    player.observe(models.EVENT.CHANGE, function (e) {
+
+        // Only update the page if the track changed
+        if (e.data.curtrack == true) {
+            updatePageWithTrackDetails();
+        }
+    });
+}
+
+function updatePageWithTrackDetails() {
+
+    var header = document.getElementById("track");
+
+    // This will be null if nothing is playing.
+    var playerTrackInfo = player.track;
+
+    if (playerTrackInfo == null) {
+        header.innerText = "Nothing playing!";
+    } else {
+        var track = playerTrackInfo.data;
+        header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
+    }
+}
