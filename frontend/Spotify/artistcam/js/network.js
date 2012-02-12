2f77de6f4e7b19c554fd04c351a099f60f7e1bfa
Petter Eriksson <periksson@periksson-mbp15-2.local>
2012-02-11 22:31:09 -0800

    Refactored network.js to call servicebase.js for generic url fetching.

--- frontend/Spotify/artistcam/js/network.js
+++ frontend/Spotify/artistcam/js/network.js
@@ -2,6 +2,7 @@ function AsyncSessionResolver(callback) {
 	this.callback = callback;
 
 	this.baseURL = "http://warm-samurai-3635.herokuapp.com";
+	this.serviceBase = new ServiceBase();
 	
 	var sessionId = "";
 	var token = "";
@@ -14,7 +15,7 @@ function AsyncSessionResolver(callback) {
 
 	this.sessionIdWithArtistURI = function (artistURI) {
 		var url = this.baseURL.concat("/" + artistURI + "/session" );
-		this.fetchURL(url, this.sessionCallback);
+		this.serviceBase.fetchURL(url, this, this.sessionCallback);
 	}
 
 	this.sessionCallback = function (responseText, self) {
@@ -25,49 +26,13 @@ function AsyncSessionResolver(callback) {
 
 	this.tokenWithSessionId = function (sessionId) {
 		var url = this.baseURL.concat("/" + sessionId + "/token" );
-		this.fetchURL(url, this.tokenCallback);
+		this.serviceBase.fetchURL(url, this, this.tokenCallback);
 	}
 
 	this.tokenCallback = function (responseText, self) {
 		token = responseText;
 		fetching = false;
 		self.callback(sessionId, token, artistURI);
-	}
-
-	this.errorCallback = function (statusText) {
-		console.log("ERROR status: " + statusText);
-	}
-
-	this.fetchURL = function (url, callback) {
-		var self = this;
-		var http = getHTTPObject();
-		console.log("url: " + url);
-    http.open("GET",url,true);
-		http.onreadystatechange = function(){
-			if (http.readyState==4){
-				console.log("http status: " + http.status + ", http.responseText: " + http.responseText);
-					if (http.status==200){
-							callback(http.responseText, self);
-            } else {
-							self.errorCallback(http.statusText);
-            }
-        }
-    }
-    http.send(null);
-	}
-
-	function getHTTPObject() {
-    if (typeof XMLHttpRequest != 'undefined') {
-        return new XMLHttpRequest();
-    }
-    try {
-        return new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-        try {
-            return new ActiveXObject("Microsoft.XMLHTTP");
-        } catch (e) {}
-    }
-    return false;
 	}
 
 }
---
+++ frontend/Spotify/artistcam/js/servicebase.js
@@ -0,0 +1,40 @@
+function ServiceBase() {
+	
+	this.fetchURL = function (url, caller, callback) {
+		var self = this;
+		var http = this.getHTTPObject();
+		console.log("url: " + url);
+    http.open("GET",url,true);
+		http.onreadystatechange = function(){
+			if (http.readyState==4){
+				console.log("http status: " + http.status + ", http.responseText: " + http.responseText);
+					if (http.status==200){
+							callback(http.responseText, caller);
+            } else {
+							self.errorCallback(http.statusText);
+            }
+        }
+    }
+    http.send(null);
+	}
+	
+	this.errorCallback = function (statusText) {
+		console.log("ERROR status: " + statusText);
+	}
+	
+	this.getHTTPObject = function () {
+    if (typeof XMLHttpRequest != 'undefined') {
+        return new XMLHttpRequest();
+    }
+    try {
+        return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {}
+    }
+    return false;
+	}
+
+
+}
