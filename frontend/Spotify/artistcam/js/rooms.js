a9c270bb976e001ef93a30ded69b9d91d7171a95
Emre Berge Ergenekon <emre.b.ergenekon@gmail.com>
2012-02-12 07:16:39 -0800

    Come on now :D

---
+++ frontend/Spotify/artistcam/js/rooms.js
@@ -0,0 +1,35 @@
+var base = new Firebase('http://gamma.firebase.com/songroulette');
+var roomCallBackFunction;
+var myUserID;
+var myRoomID;
+
+function getRoom(roomID) {
+    return  base.child('Rooms').child(roomID); 
+}
+
+
+function startTrackingRoom(userID, roomID, roomChanged, trackId) {
+    room = getRoom(roomID);
+    roomCallBackFunction = function(childSnapshot){
+        var value = childSnapshot.val();
+        if (value != null)
+            roomChanged(value);
+    };
+    
+    room.on('value', roomCallBackFunction);
+    myUserID = userID;
+    myRoomID = roomID;
+    songChanged(trackId);
+}
+
+function stopTrackingRoom() {
+    room = getRoom(myRoomID);
+    room.off('value', roomCallBackFunction);
+    room.child(myUserID).remove();
+}
+
+function songChanged(trackId) {
+    getRoom(myRoomID).child(myUserID)set({track:trackId});
+
+}
+
