ba258eaa4afa3bb31cb2a602669b3a4651a52219
Emre Berge Ergenekon <emre.b.ergenekon@gmail.com>
2012-02-12 01:53:24 -0800

    Removed log messages

--- frontend/Spotify/artistcam/js/chat.js
+++ frontend/Spotify/artistcam/js/chat.js
@@ -5,14 +5,12 @@ function getChatForRoom(roomName) {
 }
 
 function startChat(roomName, displayName) {
-    console.log("Joining chat room");
     chat = getChatForRoom(roomName);
     // We use on('child_added') to be notified when new children objects are added to the chat.
     chat.on('child_added', function(childSnapshot) {
     // childSnapshot is the added object.  We'll extract the value and use it to append to
     // our messagesDiv.
     var message = childSnapshot.val();
-    console.log("Getting msg");
     $("#messagesDiv").append("<em>" + message.name + "</em>: " + message.text + "<br />");
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
   });
@@ -26,13 +24,11 @@ function startChat(roomName, displayName) {
         text:$("#messageInput").val()
       });
       $("#messageInput").val("");
-                              console.log("Sending msg");
     }
   });
 }
 
 function stopChat(roomName) {
-    console.log("Leaving chat room");
     getChatForRoom(roomName).off('child_added', function(){})
     $('#messageInput').unbind('keypress');
     $("#messagesDiv").html("")
